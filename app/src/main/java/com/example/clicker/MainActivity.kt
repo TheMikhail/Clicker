5ed66329863e69edc635c72339874b300fb32aa2
TheMikhail <mdd2002@mail.ru>
2024-09-30 18:47:11 +0700

    Lessons 1

--- app/src/main/java/com/example/clicker/MainActivity.kt
+++ app/src/main/java/com/example/clicker/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.clicker
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var buttonClick: Button
+    private lateinit var textScore: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        buttonClick = findViewById(R.id.buttonScore)
+        textScore = findViewById(R.id.text_view)
+
+        buttonClick.setOnClickListener {
+            textScore.text = "Hello"
         }
+
     }
 }
