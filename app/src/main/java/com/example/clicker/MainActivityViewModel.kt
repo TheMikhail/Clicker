7b71914153c9dbe392ac825b2c90e38baff2e010
TheMikhail <mdd2002@mail.ru>
2024-10-11 02:44:02 +0700

    New Clicker

--- app/src/main/java/com/example/clicker/MainActivityViewModel.kt
+++ app/src/main/java/com/example/clicker/MainActivityViewModel.kt
@@ -3,22 +3,49 @@ package com.example.clicker
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class MainActivityViewModel : ViewModel() {
-    private val _countMoney = MutableStateFlow(0)
-    val countMoney: StateFlow<Int> = _countMoney
 
-    private var sizeClick = 1
-    private val updateCostImprovement = UpdateCostImprovement()
-    fun handleClickAction(increment:Int){
-        _countMoney.value += increment
+    private val autoClickManager: UpgradeManager = UpgradeAutoClickManagerImpl
+    private val multiClickManager: UpgradeManager = UpgradeMultiClickManagerImpl
+
+    private val _state = MutableStateFlow(
+        ViewState(
+            0,
+            multiClickManager.getCostUpgrade(),
+            autoClickManager.getCostUpgrade()
+        )
+    )
+    val state: StateFlow<ViewState> = _state.asStateFlow()
+
+    fun handleClick() {
+        val increment = multiClickManager.getLevel()
+        _state.update { it.copy(coin = it.coin + increment) }
     }
-    fun clickOnTap():Int{
-        return sizeClick
+
+    fun handleUpgradeMultiClick() {
+        //сюда можно дополнительно валидатор
+        val cost = multiClickManager.getCostUpgrade()
+        multiClickManager.upgrade()
+        _state.update {
+            it.copy(
+                coin = it.coin - cost,
+                multiClickCost = multiClickManager.getCostUpgrade()
+            )
+        }
     }
-    fun handleButtonMulti(){
-        _countMoney.value -= updateCostImprovement.getUpdateCostMulti()
-        updateCostImprovement.buyUpdateCountMulti()
-        sizeClick *= 2
+
+    fun handleUpgradeAutoClick() {
+        //сюда можно дополнительно валидатор
+        val cost = autoClickManager.getCostUpgrade()
+        autoClickManager.upgrade()
+        _state.update {
+            it.copy(
+                coin = it.coin - cost,
+                autoClickCost = autoClickManager.getCostUpgrade()
+            )
+        }
     }
 }
